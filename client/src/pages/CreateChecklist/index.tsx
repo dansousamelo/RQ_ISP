d49a6b9fa9e51f1ec0a6ac4055bc0ed3609da3bd
Daniel Veras <daniel_sousa.unb@outlook.com>
2023-11-15 16:31:17 -0300

    feat: add ManagementChecklist page

---
+++ client/src/pages/CreateChecklist/index.tsx
@@ -0,0 +1,53 @@
+import { Link, useNavigate } from 'react-router-dom'
+import createChecklistPicture from '../../assets/createImage.png'
+import * as S from './styles'
+import { HeaderWithoutAuth } from '../../components/HeaderWithoutAuth'
+import { MainContent } from '../../components/MainContent/components'
+import { Footer } from '../../components/Footer'
+import { highlightText } from '../../utils/highlightText'
+import { useHeaderContext } from '../../contexts/HeaderContext'
+
+export function CreateCheklist() {
+  const navigate = useNavigate()
+  const { handleIconClick, hasAddAnimation } = useHeaderContext()
+
+  const paragraphText = `Utilize um template de lista de verificação e garanta que nenhum
+  detalhe seja deixado para trás durante suas inspeções. Se já possui um
+  código acesso acesse sua lista de inspeção.`
+
+  const highlightButtonText = highlightText({
+    fullText: paragraphText,
+    textToHighlight: 'lista de inspeção',
+    color: '#34CDFF',
+    onTextToHighlightClick: () => {
+      handleIconClick('list')
+      navigate('/list')
+    },
+  })
+
+  return (
+    <S.HomePageContainer>
+      <HeaderWithoutAuth />
+      <S.MainContainer
+        initial={hasAddAnimation ? { x: '100%' } : false}
+        animate={{ x: 0 }}
+        exit={{ x: '100%' }}
+      >
+        <MainContent.Root>
+          <MainContent.Image
+            src={createChecklistPicture}
+            alt="Create checklist picture"
+          />
+          <S.ContentWrapper>
+            <MainContent.Heading>Começar uma inspeção</MainContent.Heading>
+            <MainContent.Paragraph>{highlightButtonText}</MainContent.Paragraph>
+            <Link to="/form">
+              <S.ButtonStyled>Iniciar inspeção</S.ButtonStyled>
+            </Link>
+          </S.ContentWrapper>
+        </MainContent.Root>
+      </S.MainContainer>
+      <Footer />
+    </S.HomePageContainer>
+  )
+}
