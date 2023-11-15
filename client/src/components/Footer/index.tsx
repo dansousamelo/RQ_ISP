2fb1299245c0afe2513386cf39b6de08deae26b6
owhenrique <daniel_sousa.unb@outlook.com>
2023-11-15 16:27:03 -0300

    feat: add CreateCheklist page

---
+++ client/src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+import * as S from './styles'
+
+export function Footer() {
+  return (
+    <S.StyledFooter>
+      <S.Text>Daniel de Sousa Oliveira Melo Veras</S.Text>
+      <S.Text>•</S.Text>
+      <S.Text>Paulo Henrique Almeida</S.Text>
+    </S.StyledFooter>
+  )
+}
---
+++ client/src/components/MainContent/components/index.tsx
@@ -0,0 +1,11 @@
+import { Root } from './Root'
+import { Heading } from './Heading'
+import { Paragraph } from './Paragraph'
+import { Image } from './Image'
+
+export const MainContent = {
+  Root,
+  Image,
+  Heading,
+  Paragraph,
+}
