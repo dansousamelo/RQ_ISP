f9b04ecf29f07469df827a668b6ffc84163376e8
Daniel Veras <daniel_sousa.unb@outlook.com>
2024-01-30 23:55:07 -0300

    refactor: add type guard in upload-files route

--- server/src/interfaces/type_guards.ts
+++ server/src/interfaces/type_guards.ts
@@ -1,3 +1,7 @@
 export function isString(value: unknown): value is string {
   return typeof value === "string";
 }
+
+export function isMulterFilesArray(files: any): files is globalThis.Express.Multer.File[] {
+  return Array.isArray(files) && files.every(file => typeof file === 'object' && 'fieldname' in file);
+}
