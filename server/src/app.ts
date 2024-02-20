bc27b858469b6fe2d001800412a7a1afddfff833
owhenrique <daniel_sousa.unb@outlook.com>
2024-02-20 16:08:44 -0300

    refactor: add token and file controllers

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,25 +1,20 @@
 import Express from "express";
 import { Request, Response } from "express"
 import user_controllers from "./controllers/user/user_controllers";
-import jwt from "jsonwebtoken";
-import crypto from "crypto";
-import { generateToken } from "./services/auth/auth_services";
 import dotenv from "dotenv";
 import cors from "cors";
 
-import FileServices from "./services/file/file_services";
+import token_controllers from "./controllers/token/token_controllers";
+
 import multer from "multer";
-import { Multer } from "multer";
 import multerConfig from "./config/multer";
-import { isMulterFilesArray } from "./interfaces/type_guards"
+import file_controller from "./controllers/file/file_controller";
+
 import inspection_controllers from "./controllers/inspection/inspection_controllers";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 8000;
-
-const secretKey =
-  process.env.TOKEN_SECRET_KEY || crypto.randomBytes(32).toString("hex");
 
 const app = Express();
 app.use(Express.json());
@@ -32,96 +27,15 @@ app.get("/", (req, res) => {
 
 // Rotas de Usuário
 app.post("/create-user", user_controllers.createUser);
+app.get("/generate-access_code", user_controllers.createAccessCode);
 app.get("/find-user", user_controllers.findUser);
 
 // Rotas de Serviço
-app.post("/refresh-token", async (req: Request, res: Response) => {
-  const { refreshToken } = req.body;
-
-  if (!refreshToken) {
-    return res.status(401).json({
-      error: false,
-      status: 401,
-      message: "refreshToken não informado",
-      data: {},
-    });
-  }
-
-  try {
-    const decoded = jwt.verify(refreshToken, secretKey) as
-      | { user_id: string }
-      | undefined;
-
-    if (!decoded || !decoded.user_id) {
-      return res.status(401).json({
-        error: true,
-        status: 201,
-        message: "Formato inválido de token de acesso",
-        data: {},
-      });
-    }
-
-    const newToken = generateToken(decoded.user_id);
-
-    return res.status(200).json({
-      error: false,
-      status: 200,
-      message: "New Refresh Token generated",
-      data: {
-        token: newToken,
-      },
-    });
-  } catch (error: any) {
-    console.error(error);
-    return res.status(500).json({
-      error: true,
-      status: 500,
-      message: error.message,
-      data: {},
-    });
-  }
-});
+app.post("/refresh-token", token_controllers.generateRefreshToken);
 
 // rotas de arquivo
 const upload = multer(multerConfig);
-app.post("/upload-file", upload.array("files"), async (req, res) => {
-  try {
-    const files = req.files as globalThis.Express.Multer.File[];
-    
-    if (!files || files.length === 0) {
-      throw new Error("Nenhum arquivo enviado!");
-    }
-
-    if(!isMulterFilesArray(files)){
-      throw new Error("Formato inválido de arquivo enviado!");
-    }
-
-    const fileServices = new FileServices();
-
-    const uploadedFiles = [];
-
-    for (const file of files) {
-      const { fileName, fileUrl, fileType } = await fileServices.uploadFile(file);
-      uploadedFiles.push({ fileName, fileUrl, fileType });
-    }
-
-    return res.status(200).json({
-      error: false,
-      status: 200,
-      message: "Arquivos submetidos com sucesso!",
-      data: uploadedFiles,
-    });
-
-  } catch (error) {
-    console.error("Erro durante o upload ", error);
-    return res.status(500).json({
-      error: true,
-      status: 500,
-      message: "Erro durante o upload",
-      data: {},
-    });
-  }
-});
+app.post("/upload-file", upload.array("files"), file_controller.uploadFile);
 
 //rotas de inspeção
 app.post("/create-inspection", inspection_controllers.createInspection);
