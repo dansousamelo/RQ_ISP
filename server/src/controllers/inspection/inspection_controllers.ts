a60b41f3f3faafbb2fb9adf5ba9b8620f35d9e03
owhenrique <daniel_sousa.unb@outlook.com>
2024-02-23 16:13:18 -0300

    feat: add inspection header route

--- server/src/controllers/inspection/inspection_controllers.ts
+++ server/src/controllers/inspection/inspection_controllers.ts
@@ -22,9 +22,11 @@ import {
   Item,
   Template,
   Trail,
+  Document,
 } from "../../interfaces/types";
 
 import { getErrorMessage } from "../../utils/error";
+import { DocumentItem } from "aws-sdk/clients/marketplaceagreement";
 
 export default {
   async createFirstInspection(req: Request, res: Response) {
@@ -636,97 +638,105 @@ export default {
       });
     }
   },
+
   async findInspectionAttributes(req: Request, res: Response) {
     try {
-      // const { accessCode, inspectionId } = req.query;
+      const { accessCode, inspectionId } = req.query;
 
-      // if (!isString(accessCode)) {
-      //   return res.status(400).json({
-      //     error: true,
-      //     status: 400,
-      //     message: "Forneça um código de acesso válido!",
-      //     data: {},
-      //   });
-      // }
+      if (!isString(accessCode)) {
+        return res.status(400).json({
+          error: true,
+          status: 400,
+          message: "Forneça um código de acesso válido!",
+          data: {},
+        });
+      }
 
-      // const user = await verifyUser(accessCode);
+      const user = await verifyUser(accessCode);
 
-      // if (!user) {
-      //   return res.status(404).json({
-      //     error: true,
-      //     status: 404,
-      //     message: "Usuário não encontrado!",
-      //     data: {},
-      //   });
-      // }
+      if (!user) {
+        return res.status(404).json({
+          error: true,
+          status: 404,
+          message: "Usuário não encontrado!",
+          data: {},
+        });
+      }
 
-      // if (!isString(inspectionId)) {
-      //   return res.status(400).json({
-      //     error: true,
-      //     status: 400,
-      //     message: "Forneça um id de inspeção válido!",
-      //     data: {},
-      //   });
-      // }
+      if (!isString(inspectionId)) {
+        return res.status(400).json({
+          error: true,
+          status: 400,
+          message: "Forneça um id de inspeção válido!",
+          data: {},
+        });
+      }
 
-      // let inspection: Inspection | null;
+      let inspection: Inspection | null;
 
-      // try {
-      //   inspection = await prisma.inspection.findFirst({
-      //     where: {
-      //       id: inspectionId,
-      //     },
-      //     include: {
-      //       Document: true,
-      //     },
-      //   });
-      // } catch (error) {
-      //   return res.status(500).json({
-      //     error: true,
-      //     status: 500,
-      //     message: getErrorMessage(error),
-      //     data: {},
-      //   });
-      // }
+      try {
+        inspection = await prisma.inspection.findFirst({
+          where: {
+            id: inspectionId,
+          },
+          include: {
+            Document: true,
+          },
+        });
+      } catch (error) {
+        return res.status(500).json({
+          error: true,
+          status: 500,
+          message: getErrorMessage(error),
+          data: {},
+        });
+      }
 
-      // if (!inspection) {
-      //   return res.status(404).json({
-      //     error: true,
-      //     status: 404,
-      //     message: "Inspeção não encontrada!",
-      //     data: {},
-      //   });
-      // }
+      if (!inspection) {
+        return res.status(404).json({
+          error: true,
+          status: 404,
+          message: "Inspeção não encontrada!",
+          data: {},
+        });
+      }
 
-      // let inspectionDocuments: DocumentItems[] = [];
+      if (!inspection) {
+        return res.status(404).json({
+          error: true,
+          status: 404,
+          message: "Inspeção não encontrada!",
+          data: {},
+        });
+      }
 
-      // if (inspection.document) {
-      //   inspectionDocuments = inspection.document.map((doc: DocumentItems) => ({
-      //     fileName: doc.fileName,
-      //     fileType: doc.fileType,
-      //     fileUrl: doc.fileUrl,
-      //   }));
-      // }
+      const inspectionDocuments = inspection.Document
+        ? inspection.Document.map((doc: Document) => ({
+            fileName: doc.name,
+            fileType: doc.type,
+            fileUrl: doc.url,
+          }))
+        : [];
 
-      // const inspectionsData = {
-      //   name: inspection.name,
-      //   responsible: inspection.responsible,
-      //   type: inspection.type,
-      //   recording_url: inspection.recording_url,
-      //   participants: inspection.participants,
-      //   responsible_email: inspection.responsible_email,
-      //   documents: inspectionDocuments,
-      //   status: inspection.status,
-      // };
+      const inspectionsData = {
+        name: inspection.name,
+        responsible: inspection.responsible,
+        type: inspection.type,
+        recording_url: inspection.recording_url,
+        participants: inspection.participants,
+        responsible_email: inspection.responsible_email,
+        documents: inspectionDocuments,
+        status: inspection.status,
+      };
 
-      // return res.status(200).json({
-      //   error: false,
-      //   status: 200,
-      //   message: "Dados da inspeção encontrados com sucesso!",
-      //   data: {
-      //     inspection: inspectionsData,
-      //   },
-      // });
+      return res.status(200).json({
+        error: false,
+        status: 200,
+        message: "Dados da inspeção encontrados com sucesso!",
+        data: {
+          inspection: inspectionsData,
+        },
+      });
     } catch (error) {
       return res.status(500).json({
         error: true,
