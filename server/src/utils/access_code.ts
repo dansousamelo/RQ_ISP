59039d093b9cdbb10125916d2edaebdb7073e6d0
owhenrique <daniel_sousa.unb@outlook.com>
2024-02-25 17:13:57 -0300

    refactor: project architecture

--- server/src/utils/access_code.ts
+++
@@ -1,9 +0,0 @@
-import { randomBytes } from "crypto";
-
-export function generateAccessCode(length: number): string {
-    const bytes = Math.ceil(length * 0.75);
-    let accessCode = randomBytes(bytes).toString('base64').slice(0, length);
-
-    accessCode = accessCode.replace(/\//g, '-').replace(/\+/g, '-');
-    return accessCode;
-}
