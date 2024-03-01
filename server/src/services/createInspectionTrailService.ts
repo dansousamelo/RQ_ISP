7197496de02acd35f2327ad5b200524a1df91323
owhenrique <me.pauloalmeida@gmail.com>
2024-03-01 01:50:05 -0300

    feat: trail deleting route

--- server/src/services/createInspectionTrailService.ts
+++ server/src/services/createInspectionTrailService.ts
@@ -3,41 +3,107 @@ import { getErrorMessage } from "../utils/errorMessage";
 
 import { PositionRect, TrailData } from "./interfaces/types";
 
-export async function createInspectionTrail(
-  inspectionId: string,
-  trailData: TrailData
-) {
+async function findTextTrail(inspectionId: string, itemIndex: string) {
   try {
-    const trail = await createTrailInstance(inspectionId, trailData);
+    const findedTextTrail = await prisma.textTrail.findFirst({
+      where: {
+        inspectionId: inspectionId,
+        itemIndex: itemIndex,
+      },
+    });
 
-    await Promise.all([
-      createTrailBoundingRect(trail.id, trailData.position.boundingRect),
-      createTrailRects(trail.id, trailData.position.rects),
-    ]);
+    if (!findedTextTrail) {
+      return null;
+    }
+
+    return findedTextTrail.id;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function createTextTrail(inspectionId: string, itemIndex: string, trailData: string) {
+  try {
+    const trail = await prisma.textTrail.create({
+      data: {
+        inspectionId: inspectionId,
+        itemIndex: itemIndex,
+        text: trailData,
+      },
+    });
 
     return trail.id;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function createDocumentTrail(inspectionId: string,documentId: string, trailData: TrailData) {
+  try {
+    const itemIndex = trailData.comment.text;
+    const findedTextTrail = await findTextTrail(inspectionId, itemIndex);
+
+    if (findedTextTrail) {
+      await destroiTextTrail(findedTextTrail);
+    }
+
+    const documentTrail = await createDocumentTrailInstance(inspectionId, documentId,itemIndex, trailData.content.text);
+
+    const documentTrailPosition = await createDocumentTrailPosition(documentTrail,trailData.position.pageNumber);
+
+    await Promise.all([
+      createTrailBoundingRect(
+        documentTrailPosition,
+        trailData.position.boundingRect
+      ),
+      createTrailRects(documentTrailPosition, trailData.position.rects),
+    ]);
+
+    return documentTrail;
   } catch (error) {
     throw new Error(getErrorMessage(error));
   }
 }
 
-async function createTrailInstance(inspectionId: string, trail: TrailData) {
+async function createDocumentTrailInstance(
+  inspectionId: string,
+  documentId: string,
+  itemIndex: string,
+  text: string,
+) {
   try {
-    const { comment, content } = trail;
-    const { pageNumber } = trail.position;
 
-    const createdTrail = await prisma.trail.create({
+    const createdTrail = await prisma.documentTrail.create({
       data: {
-        inspection_id: inspectionId,
-        text: content.text,
-        item_id: comment.text,
-        page_number: pageNumber,
+        inspectionId: inspectionId,
+        documentId: documentId,
+        itemIndex: itemIndex,
+        text: text,
       },
     });
 
-    return createdTrail;
+    return createdTrail.id;
   } catch (error) {
     throw new Error("Não foi possível inserir um objeto na tabela marcação!");
+  }
+}
+
+async function createDocumentTrailPosition(
+  documentTrailId: string,
+  pageNumber: number
+) {
+  try {
+    const createdDocumentTrailPosition =
+      await prisma.documentTrailPosition.create({
+        data: {
+          documentTrailId: documentTrailId,
+          pageNumber: pageNumber,
+        },
+      });
+
+    return createdDocumentTrailPosition.id;
+  } catch (error) {
+    throw error;
   }
 }
 
@@ -46,18 +112,19 @@ async function createTrailBoundingRect(
   boundingRect: PositionRect
 ) {
   try {
-    const createdBoundingRect = await prisma.bouding_Rect.create({
-      data: {
-        trail_id: trailId,
-        x1: boundingRect.x1.toString(),
-        x2: boundingRect.x2.toString(),
-        y1: boundingRect.y1.toString(),
-        y2: boundingRect.y2.toString(),
-        width: boundingRect.width.toString(),
-        height: boundingRect.height.toString(),
-        page_number: boundingRect.pageNumber,
-      },
-    });
+    const createdBoundingRect =
+      await prisma.documentTrailPositionBoudingRect.create({
+        data: {
+          documentTrailPositionId: trailId,
+          x1: boundingRect.x1.toString(),
+          x2: boundingRect.x2.toString(),
+          y1: boundingRect.y1.toString(),
+          y2: boundingRect.y2.toString(),
+          width: boundingRect.width.toString(),
+          height: boundingRect.height.toString(),
+          pageNumber: boundingRect.pageNumber,
+        },
+      });
 
     return createdBoundingRect;
   } catch (error) {
@@ -67,25 +134,37 @@ async function createTrailBoundingRect(
 
 async function createTrailRects(trailId: string, rects: PositionRect[]) {
   try {
-    const createRectPromises = rects.map((rect: PositionRect) => {
-      return prisma.trail_Rects.create({
-        data: {
-          trail_id: trailId,
-          x1: rect.x1.toString(),
-          x2: rect.x2.toString(),
-          y1: rect.y1.toString(),
-          y2: rect.y2.toString(),
-          width: rect.width.toString(),
-          height: rect.height.toString(),
-          page_number: rect.pageNumber,
-        },
-      });
-    });
-
-    const createdRects = await Promise.all(createRectPromises);
+    const createdRects = await Promise.all(
+      rects.map((rect: PositionRect) => {
+        return prisma.documentTrailPositionRect.create({
+          data: {
+            documentTrailPositionId: trailId,
+            x1: rect.x1.toString(),
+            x2: rect.x2.toString(),
+            y1: rect.y1.toString(),
+            y2: rect.y2.toString(),
+            width: rect.width.toString(),
+            height: rect.height.toString(),
+            pageNumber: rect.pageNumber,
+          },
+        });
+      })
+    );
 
     return createdRects;
   } catch (error) {
     throw new Error(getErrorMessage(error));
+  }
+}
+
+async function destroiTextTrail(textTrailId: string) {
+  try {
+    await prisma.textTrail.delete({
+      where: {
+        id: textTrailId,
+      },
+    });
+  } catch (error) {
+    throw error;
   }
 }
