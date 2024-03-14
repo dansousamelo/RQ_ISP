7915a837586ac1205f5d55acea6e2c36d3685f30
owhenrique <me.pauloalmeida@gmail.com>
2024-03-13 21:51:23 -0300

    feat: add route to get items statistics

--- server/src/controllers/chartControllers.ts
+++ server/src/controllers/chartControllers.ts
@@ -1,10 +1,10 @@
 import { Request, Response } from "express"
 
-import { findItemsCategoriesByInspectionId } from "../services/chartServices";
+import { findItemsCategoriesByInspectionId, findItemsSituationStatisticsByInspectionId } from "../services/chartServices";
 
 import { getErrorMessage } from "../utils/errorMessage";
 
-import { isString } from "../interfaces/typeGuards";
+import { isCategoryValid, isString } from "../interfaces/typeGuards";
 
 export default {
     async findInspectionCategories(req: Request, res: Response){
@@ -28,6 +28,50 @@ export default {
                 message: "Categorias encontradas com sucesso",
                 data: {
                     categories
+                }
+            })
+            
+        } catch (error) {
+            return res.status(500).json({
+                error: true,
+                status: 500,
+                message: getErrorMessage(error),
+                data: {},
+            });
+        }
+    },
+
+    async findInspectionItemsSituationsStatistics(req: Request, res: Response) {
+        try {
+            const { inspectionId, category } = req.query
+
+            if(!isString(inspectionId)){
+                return res.status(400).json({
+                    error: true,
+                    status: 400,
+                    message: "Forneça um id de inspeção válido!",
+                    data: {}
+                })
+            }
+
+            if(!isString(category) || !isCategoryValid(category)){
+                return res.status(400).json({
+                    error: true,
+                    status: 400,
+                    message: "Forneça uma categoria de inspeção válida!",
+                    data: {}
+                })
+            }
+
+            const statistics = await findItemsSituationStatisticsByInspectionId(inspectionId, category)
+
+            return res.status(200).json({
+                error: false,
+                status: 200,
+                message: "",
+                data: {
+                    category: category,
+                    statistics
                 }
             })
             
--- server/src/services/chartServices.ts
+++ server/src/services/chartServices.ts
@@ -1,3 +1,4 @@
+import { CONCLUDED, GENERAL } from "../constants/constants";
 import { prisma } from "../db/prismaClient";
 
 function translateCategory(category: string): string {
@@ -48,4 +49,54 @@ export async function findItemsCategoriesByInspectionId(inspectionId: string) {
     } catch (error) {
       throw error
     }
+}
+
+export async function findItemsSituationStatisticsByInspectionId(inspectionId: string, category: string) {
+  try {
+    const inspection = await prisma.inspection.findUnique({
+      where: {
+          id: inspectionId,
+      },
+      include: {
+          item: {
+              where: category !== 'general' ? { category } : undefined,
+          },
+      },
+  });
+
+  if (!inspection) {
+      throw new Error("Não foi possível encontrar uma inspeção com este id!");
+  }
+
+  if(inspection.status !== CONCLUDED) {
+    throw new Error("Essa inspeção não está concluída, conclua para que seja possível gerar as estatísticas!")
+  }
+
+  const situationOptions = [
+      { value: 'as_per', label: 'Conforme' },
+      { value: 'incomplete', label: 'Incompleto' },
+      { value: 'non_compilant', label: 'Não conforme' },
+      { value: 'not_applicable', label: 'Não se aplica' },
+  ];
+
+  const initialStatistics = Object.fromEntries(situationOptions.map(option => [option.value, 0]));
+
+  const allSituations = inspection.item.flatMap(item => item.situation);
+
+  const statistics = allSituations.reduce((acc, situation) => {
+      if (situation && situation in acc) {
+          acc[situation]++;
+      }
+      return acc;
+  }, { ...initialStatistics });
+
+  const result = situationOptions.map(option => ({
+      ...option,
+      value: statistics[option.value],
+  }));
+
+  return result;
+  } catch (error) {
+    throw error
+  }
 }
