0845fbb5425ea9f7a2a7cc423d8da4f9e1767d81
Daniel Veras <daniel_sousa.unb@outlook.com>
2024-03-20 20:21:09 -0300

    refactor: refactor package.json

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -1,5 +1,11 @@
 import axios from 'axios'
 
+let baseURL = 'http://localhost:8000'
+
+if (process.env.NODE_ENV === 'production') {
+  baseURL = 'https://rq-isp-server.vercel.app/'
+}
+
 export const api = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL,
 })
