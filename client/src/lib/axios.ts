0d48d996ddaec5f88452fa0cd369be79450efa6e
Daniel Veras <daniel_sousa.unb@outlook.com>
2024-03-20 21:31:12 -0300

    refactor: refactor axios library

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 let baseURL = 'http://localhost:8000'
 
 if (process.env.NODE_ENV === 'production') {
-  baseURL = 'https://rq-isp-server.vercel.app'
+  baseURL = 'https://rq-isp-2.onrender.com/'
 }
 
 export const api = axios.create({
