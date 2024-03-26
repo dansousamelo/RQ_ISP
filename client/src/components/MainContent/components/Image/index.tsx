2ed2c3403d53e4f22d1f28bf2873cfddb02677c2
Daniel Veras <daniel_sousa.unb@outlook.com>
2024-03-26 18:09:21 -0300

    refactor: refactor load images

--- client/src/components/MainContent/components/Image/index.tsx
+++ client/src/components/MainContent/components/Image/index.tsx
@@ -1,5 +1,24 @@
+import React, { useState } from 'react'
 import * as S from './styles'
 
 export function Image(props: React.HTMLProps<HTMLImageElement>) {
-  return <S.Image {...props} />
+  const { src, alt, ...rest } = props
+  const [imageLoaded, setImageLoaded] = useState(false)
+
+  const handleImageLoad = () => {
+    setImageLoaded(true)
+  }
+
+  return (
+    <>
+      {!imageLoaded && <S.ImageSkeleton />}
+      <S.Image
+        {...rest}
+        src={src}
+        alt={alt}
+        onLoad={handleImageLoad}
+        style={{ display: imageLoaded ? 'block' : 'none' }}
+      />
+    </>
+  )
 }
--- client/src/components/MainContent/components/Image/styles.ts
+++ client/src/components/MainContent/components/Image/styles.ts
@@ -1,8 +1,16 @@
 import { styled } from 'styled-components'
+import { Skeleton } from '../../../Skeleton'
 
 export const Image = styled.img`
-  width: 45%;
-  height: auto;
+  height: 442.8px;
+  width: 442.8px;
   object-fit: contain;
   max-width: 100%;
+`
+
+export const ImageSkeleton = styled(Skeleton)`
+  height: 442.8px;
+  width: 442.8px;
+  max-width: 100%;
+  border-radius: 8px;
 `
