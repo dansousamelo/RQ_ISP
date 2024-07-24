ebe3196b7aa40561204226cc90758189d9a68d55
Daniel Veras <daniel_sousa.unb@outlook.com>
2024-07-24 17:35:12 -0300

    feature: add cookie logic

---
+++ client/src/components/CookieConsent/index.tsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from 'react'
+import * as Dialog from '@radix-ui/react-dialog'
+import Cookies from 'js-cookie'
+import * as S from './styles'
+
+const COOKIE_CONSENT_NAME = 'cookie_consent'
+
+export const CookieConsent = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  useEffect(() => {
+    const consent = Cookies.get(COOKIE_CONSENT_NAME)
+    if (!consent) {
+      setIsOpen(true)
+    }
+  }, [])
+
+  const handleAccept = () => {
+    Cookies.set(COOKIE_CONSENT_NAME, 'true', { expires: 365 })
+    setIsOpen(false)
+  }
+
+  return (
+    <Dialog.Root open={isOpen}>
+      <Dialog.Portal>
+        <S.Overlay />
+        <S.Content>
+          <S.Wrapper>
+            <h1>Política de Cookies</h1>
+            <span>
+              Usamos cookies para garantir que você obtenha a melhor experiência
+              em nosso site. Consulte nossa Política de Privacidade.
+            </span>
+          </S.Wrapper>
+          <S.ButtonStyled onClick={handleAccept}>Aceitar</S.ButtonStyled>
+        </S.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
+  )
+}
